f92d616e3af509b6f3983ce80eeb9515c0a1bb8f
DavidMarkNjogu <davidmarknjogu@gmail.com>
2025-09-08 00:54:38 +0300

    - Hero section

---
+++ script.js
@@ -0,0 +1,23 @@
+// SECTION NAVING---NAVIGATION ie.\
+
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-ul li a');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.parentElement.classList.remove('active');
+    if (link.getAttribute('href') === '#' + current) {
+      link.parentElement.classList.add('active');
+    }
+  });
+});
